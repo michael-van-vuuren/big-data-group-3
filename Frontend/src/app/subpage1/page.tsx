54165d55d9e8ff100b70d6b74834e911123384aa
Melina Kopischkie <mkopischkie@gmail.com>
2025-03-06 15:38:13 -0700

    login page

--- Frontend/src/app/subpage1/page.tsx
+++ Frontend/src/app/subpage1/page.tsx
@@ -1,27 +1,21 @@
-"use client"
+// pages/subpage1.tsx
+import { Input } from '../../components/ui/input'
+import { Button } from '../../components/ui/button'
 
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import Visualization from '@/sections/visualization'
-import Divider from '@/components/divider'
-
-export default function SubPage1() {
+const Subpage1 = () => {
   return (
-    <>
-      <h1 className="text-3xl font-heading md:text-4xl lg:text-5xl py-6 pl-10">
-        Sub Page 1
-      </h1>
-      <Divider />
-      <Visualization />
-      <Divider>
-        <div className="px-5 py-[10px] text-center">
-          <Link href="/" passHref>
-            <Button size="lg" className="text-base font-heading md:text-lg lg:h-14 lg:text-xl">
-              Home
-            </Button>
-          </Link>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center space-y-6">
+        <h1 className="text-xl font-bold">Login before taking the quiz</h1>
+        <div className="flex flex-col items-center space-y-4">
+          <Input className="w-[200px]" type="email" placeholder="Email" />
+          <Input className="w-[200px]" type="password" placeholder="Password" />
+          {/* Submit button */}
+          <Button className="w-[200px]" type="submit">Submit</Button>
         </div>
-      </Divider>
-    </>
+      </div>
+    </div>
   )
 }
+
+export default Subpage1
