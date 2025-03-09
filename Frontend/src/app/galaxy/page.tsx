a8d6f9cee19a4aa2f42532242884e66ca658788c
michael-van-vuuren <mvanvuuren@ucla.edu>
2025-03-09 14:59:47 -0600

    added resizable side menu

--- Frontend/src/app/galaxy/page.tsx
+++ Frontend/src/app/galaxy/page.tsx
@@ -3,29 +3,53 @@
 import Galaxy from "@/sections/galaxy/galaxy";
 import { useState } from "react";
 
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
 export default function Home() {
     const [planetName, setPlanetName] = useState<string | null>(null);
 
     return (
-        <div style={{ height: "80vh", width: "100vw" }}>
-            <Galaxy setPlanetName={setPlanetName} />
-            {planetName && (
-                <div
-                    style={{
-                        position: "absolute",
-                        top: "55%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        padding: "8px 16px",
-                        backgroundColor: "black",
-                        color: "white",
-                        borderRadius: "4px",
-                        textAlign: "center",
-                    }}
+        <>
+            <div style={{ height: "80vh", width: "100vw" }}>
+                <ResizablePanelGroup
+                    direction="horizontal"
+                    className="rounded-base w-full border-2 border-border text-mtext shadow-shadow"
                 >
-                    {planetName}
-                </div>
-            )}
-        </div>
+                    {/* menu */}
+                    <ResizablePanel defaultSize={20}>
+                        <div className="flex h-full items-center justify-center bg-main p-0">
+                            {planetName ? (
+                                <div
+                                    style={{
+                                        padding: "8px 16px",
+                                        backgroundColor: "black",
+                                        color: "white",
+                                        borderRadius: "4px",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    {planetName}
+                                </div>
+                            ) : (
+                                <span className="font-base">No planet selected</span>
+                            )}
+                        </div>
+                    </ResizablePanel>
+
+                    <ResizableHandle />
+
+                    {/* galaxy scene */}
+                    <ResizablePanel defaultSize={80}>
+                        <div className="flex h-full items-center justify-center bg-main p-0">
+                            <Galaxy setPlanetName={setPlanetName} />
+                        </div>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            </div>
+        </>
     );
 }
