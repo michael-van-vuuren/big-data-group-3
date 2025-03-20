8d43439ee4ee07d6e8e4304f97fc36ed129cf72f
michael-van-vuuren <mvanvuuren@ucla.edu>
2025-03-20 13:49:12 -0600

    add timer to python import script and remove obsolete scripts

--- TempData/Scripts/Importing/import-products.py
+++ TempData/Scripts/Importing/import-products.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import requests
 import json
+import time
 
 '''
 Get unique products from JSON.
 '''
-file_path = Path(__file__).resolve().parents[2] / 'test-coffee-bean-products.json'
+file_path = Path(__file__).resolve().parents[2] / 'coffee-bean-products-v2.json'
 
 with open(file_path, 'r', encoding='utf-8') as f:
     data = json.load(f)
@@ -18,23 +19,28 @@ def add_products(payload):
     '''Add products using Backend API endpoint.'''
     url = 'http://localhost:8080/api/products/import'
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(url,
-                            headers=headers,
-                            json=payload
-                            )
+
+    start_time = time.time()
+    response = requests.post(url, headers=headers, json=payload)
+    end_time = time.time()
+
     print('Status code:', response.status_code)
     print('Response body:', response.text)
+    print(f'Time taken: {end_time - start_time:.4f} seconds')
 
 def update_products(payload):
     '''Update products using Backend API endpoint.'''
     url = 'http://localhost:8080/api/products/import'
     headers = {'Content-Type': 'application/json'}
-    response = requests.put(url,
-                            headers=headers,
-                            json=payload
-                            )
+
+    start_time = time.time()
+    response = requests.post(url, headers=headers, json=payload)
+    end_time = time.time()
+
     print('Status code:', response.status_code)
     print('Response body:', response.text)
+    print(f'Time taken: {end_time - start_time:.4f} seconds')
 
-# add_products(payload)
-update_products(payload)
+
+add_products(payload)
+# update_products(payload)
